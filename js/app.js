05d0e72dee7dfcbd0c545b78899d8f9c33b9a429
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-05 17:41:11 +0200

    read api

--- js/app.js
+++ js/app.js
@@ -9,14 +9,29 @@ function loadNames(e) {
   // Read variables
   const origin = document.getElementById('origin')
   const selectOrigin = origin.options[origin.selectedIndex].value
+
   const genre = document.getElementById('genre')
   const selectGenre = genre.options[genre.selectedIndex].value
 
+  const quantity = document.getElementById('number').value
+
   let url = ''
-  url += ''
-  
-  console.log(selectOrigin)
+  url += 'http://uinames.com/api/?'
+
+  // if there is origin add it to the url
+  if(selectOrigin !== '') {
+    url +=`region=${selectOrigin}&`
+  }
+  // if there is genre add it to the url
+  if(selectGenre !== '') {
+    url +=`genre=${selectGenre}&`
+  }
+  // if there is number add it to the url
+  if(quantity !== '') {
+    url +=`amount=${quantity}&`
+  }
+  console.log(url)
   console.log(selectGenre)
-  console.log(selectNumber)
+  console.log(number)
 
 }
